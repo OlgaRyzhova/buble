f2c94afe4616041ff15b00de59adccf53bd27ed2
Olga <miliukovao@gmail.com>
2022-10-19 21:26:13 +0300

    vue credit card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -7,7 +7,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CredirCardForm from '@/components/CredirCardForm/components/CreditCardForm.vue'
 
 export default {
   name: 'HomeView',
@@ -15,4 +15,4 @@ export default {
     HelloWorld
   }
 }
-</script>
+</script> -->
---
+++ src/views/PlayGroundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CreditCardForm/>
+  </div>
+</template>
+
+<script>
+import CreditCardForm from '../components/CredirCardForm/components/CreditCardForm.vue';
+
+  export default {
+    components: {
+      CreditCardForm
+    }
+  }
+</script>
